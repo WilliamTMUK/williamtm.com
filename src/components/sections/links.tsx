c61210dad7f552a8b2f6b175936721ceae39a031
Trevor Morris <code@trovster.com>
2025-03-23 18:21:39 +0000

    fix: title for links

--- src/components/sections/links.tsx
+++ src/components/sections/links.tsx
@@ -1,14 +1,14 @@
 import { IconType } from '@icons-pack/react-simple-icons'
 import { FC } from 'react'
 
-export default function Links({ links }: { links: { href: string; icon: IconType|FC; text: string; }[]; }) {
+export default function Links({ links }: { links: { text: string; icon: IconType|FC; href: string; title: string; }[]; }) {
   return (
     <div className="my-4">
       <ul className="space-y-4">
         {links.map((link, id) => {
           return (
             <li key={id}>
-              <a target="_blank" href={link.href} title="{link.title}" className="group inline-flex space-x-2">
+              <a target="_blank" href={link.href} title={link.title} className="group inline-flex space-x-2">
                 <link.icon title="" />
                 <span className="underline group-hover:no-underline group-focus:no-underline">{link.text}</span>
               </a>
