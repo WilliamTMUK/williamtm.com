7fd5cbe3dcd24c9282222c6013eae86f092368a5
William Hook <williamhook@gmail.com>
2025-03-23 19:56:23 +0000

    data: add metadata to gigs list

--- src/data/gigs/UPCOMING.ts
+++ src/data/gigs/UPCOMING.ts
@@ -12,22 +12,22 @@ const GIGS: {
   },
   {
     title: 'Forest Live 2025 - Nile Rodgers & CHIC, The Brand New Heavies',
-    venue: 'Cannock Chase Forest',
+    venue: 'Cannock Chase Forest, Rugeley',
     date: new Date('2025-06-25'),
   },
   {
     title: 'Forest Live 2025 - Snow Patrol',
-    venue: 'Cannock Chase Forest',
+    venue: 'Cannock Chase Forest, Rugeley',
     date: new Date('2025-06-26'),
   },
   {
     title: 'Forest Live 2025 - Sting',
-    venue: 'Cannock Chase Forest',
+    venue: 'Cannock Chase Forest, Rugeley',
     date: new Date('2025-06-27'),
   },
   {
     title: 'Forest Live 2025 - James, Razerlight, Jamie Webster',
-    venue: 'Cannock Chase Forest',
+    venue: 'Cannock Chase Forest, Rugeley',
     date: new Date('2025-06-28'),
   },
   {
