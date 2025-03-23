f321c63a29b4df65c61170427a721b25d0bb5689
Trevor Morris <code@trovster.com>
2025-03-23 09:41:18 +0000

    data: update gigs list

--- src/data/gigs.ts
+++ src/data/gigs.ts
@@ -20,6 +20,63 @@ const PAST_GIGS: {
   {
     title: 'Jimmy Carr - Rapier Wit',
   },
+  {
+    title: 'Frankie Boyle - I Would Happily Punch Every One Of You In The Face',
+  },
+  {
+    title: 'Jean-Michel Jarre',
+  },
+  {
+    title: 'Gorillaz - Plastic Beach Tour',
+  },
+  {
+    title: 'Katy Perry - California Girls Tour',
+  },
+  {
+    title: 'Coldplay - Mylo Xyloto Tour',
+  },
+  {
+    title: 'Jimmy Carr - Gagging Order',
+  },
+  {
+    title: 'The Killers - Battle Born Tour',
+  },
+  {
+    title: 'Rich Hall',
+  },
+  {
+    title: 'Russell Kane - Posturing Delivery',
+  },
+  {
+    title: 'Micky Flanagan - Back in the Game',
+  },
+  {
+    title: 'Rob Beckett',
+  },
+  {
+    title: 'Morcheeba feat. Skye',
+  },
+  {
+    title: 'Nitro Circus Live',
+  },
+  {
+    title: 'Russell Howard - Wonderbox',
+  },
+  {
+    title: 'James Blunt - Moon Landing Tour',
+  },
+  {
+    title: 'Coldplay - Ghost Stories Tour',
+  },
+  {
+    title: 'Pharrell Williams - Dear G I R L Tour',
+  },
+  {
+    title: 'CHVRCHΞS - The Bones of What You Believe',
+  },
+  {
+    title: 'John Bishop - Supersonic',
+  },
 ]
 
 const UPCOMING_GIGS: {
@@ -37,6 +94,14 @@ const UPCOMING_GIGS: {
   {
     title: 'Imagine Dragons - LOOM Tour',
     date: new Date('2025-07-01'),
+  },
+  {
+    title: 'Coldplay - Music of the Spheres Tour',
+    date: new Date('2025-08-01'),
+  },
+  {
+    title: 'The Midnight',
+    date: new Date('2025-10-01'),
   },
 ]
 
