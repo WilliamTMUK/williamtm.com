e0524e6053d51b53710b67ae83684e9e331bfd19
William Hook <williamhook@gmail.com>
2025-03-28 22:27:51 +0000

    data: missing date on gigs

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/will/Sites/williamtm.com/src/app/layout\":[\"static/media/4f05ba3a6752a328-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oVYcOVSnGlMm4hEhcJLaD6NofLZOC9KsTx/DLlQxQgk=\"\n}"
